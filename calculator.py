8531e9d145597a32e06ea082f43871cc9c17dc0b
2025sl93023 <2025sl93023@wilp.bits-pilani.ac.in>
2026-04-06 22:58:39 +0530

    Introduced bug

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def add(a,b):
-	return a+b
+	return a-b
 def multiply(a,b):
 	return a*b
 def subtract(a,b):
@@ -8,3 +8,13 @@ def divide(a,b):
 	if b==0:
 		return None
 	return a/b
+
+if __name__ == "__main__":
+    print("Running tests...")
+
+    assert add(2, 3) == 5
+    assert subtract(5, 3) == 2
+    assert multiply(2, 3) == 6
+    assert divide(6, 3) == 2
+
+    print("All tests passed successfully!")
